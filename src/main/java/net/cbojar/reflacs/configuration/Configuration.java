82c7af6fcad07b30a931cdd1920785e94c155604
cbojar <chris@cbojar.net>
2026-03-29 19:17:38 -0400

    Load configuration from destination file.

--- src/main/java/net/cbojar/reflacs/configuration/Configuration.java
+++ src/main/java/net/cbojar/reflacs/configuration/Configuration.java
@@ -1,23 +1,54 @@
 package net.cbojar.reflacs.configuration;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public final class Configuration {
-	private static final Map<String, String> OPTIONS = Map.of(
-		"output-format", "ogg",
-		"bitrate", "256K");
-
 	private final Path source;
 	private final Path destination;
+	private final Map<String, String> options;
 
-	private Configuration(final Path source, final Path destination) {
+	private Configuration(final Path source, final Path destination, final Map<String, String> options) {
 		this.source = source;
 		this.destination = destination;
+		this.options = options;
 	}
 
-	public static Configuration load(final String source, final String destination) {
-		return new Configuration(Path.of(source), Path.of(destination));
+	public static Configuration load(final String source, final String destination) throws IOException {
+		return load(Path.of(source), Path.of(destination));
+	}
+
+	public static Configuration load(final Path source, final Path destination) throws IOException {
+		return new Configuration(source, destination, readDestination(destination));
+	}
+
+	private static Map<String, String> readDestination(final Path destination) throws IOException {
+		final Path configurationFile = destination.resolve(".reflacs");
+
+		if (!Files.exists(configurationFile)) {
+			throw new IOException("Reflacs configuration file not found: " + configurationFile);
+		}
+
+		final Properties properties = new Properties();
+		try (final BufferedReader configurationReader = Files.newBufferedReader(configurationFile)) {
+			properties.load(configurationReader);
+		}
+
+		return propertiesToMap(properties);
+	}
+
+	private static Map<String, String> propertiesToMap(final Properties properties) {
+		final Map<String, String> map = new LinkedHashMap<>();
+		for (final String property : properties.stringPropertyNames()) {
+			map.put(property, properties.getProperty(property));
+		}
+
+		return map;
 	}
 
 	public Path source() {
@@ -32,11 +63,7 @@ public final class Configuration {
 		return destination.resolve(source.relativize(path));
 	}
 
-	public Map<String, String> options() {
-		return OPTIONS;
-	}
-
 	public String get(final String name) {
-		return OPTIONS.get(name);
+		return options.get(name);
 	}
 }
